0fc7e2210169f686c92f68ea1201c14880b03beb
Gad Maor <gadm@il.ibm.com>
2018-11-23 02:36:54 +0200

    Finished CheckingAccount class

--- BankAccount/src/BankAccount.java
+++ BankAccount/src/BankAccount.java
@@ -1,3 +1,6 @@
+/** Represents a BankAccount, holding a specific balance.
+ *
+ */
 public abstract class BankAccount {
 
     /******************************************************
@@ -11,6 +14,14 @@ public abstract class BankAccount {
     /******************************************************
      *                      Constructors                  *
      *****************************************************/
+    /** Create a new BankAccount object
+     *
+     * @param accountNumber This BankAccount's accountNumber
+     * @param accountOwnerName This BankAccount's accountownerName
+     * @param accountOwnerId This BankAccount's accountOwnerId
+     * @param balance This BankAccount's balance
+     * @throws IllegalBalanceException if balance < 0.0
+     */
     public BankAccount(String accountNumber, String accountOwnerName, String accountOwnerId, double balance)
             throws  IllegalBalanceException {
         this.accountNumber = accountNumber;
@@ -22,6 +33,9 @@ public abstract class BankAccount {
         this.balance = balance;
     }
 
+    /******************************************************
+     *                      Methods                       *
+     *****************************************************/
     public String getAccountNumber() {
         return accountNumber;
     }
@@ -38,6 +52,11 @@ public abstract class BankAccount {
         return balance;
     }
 
+    /** Setter method for balance
+     *
+     * @param balance The balance to which to set this BankAccount's balance
+     * @throws IllegalBalanceException if balance < 0.0
+     */
     public void setBalance(double balance) throws IllegalBalanceException {
         if (balance < 0.0) {
             throw new IllegalBalanceException("Balance must be >= 0.0");
@@ -49,9 +68,60 @@ public abstract class BankAccount {
      *
      * @param amount The amount to deposit to this BankAccount's balance
      */
-    public void deposit(double amount) throws IllegalBalanceException {
+    public void deposit(double amount) {
         if (amount >= 0.0){
-            setBalance(getBalance() + amount);
+            this.setBalance(this.getBalance() + amount);
+        }
+    }
+
+    /** Withdraws given amount from this BankAccount's balance
+     *
+     * @param amount The amount to withdraw from this BankAccount
+     * @throws IllegalBalanceException if there's not enough money in this BankAccount to withdraw
+     */
+    public void withdraw(double amount) throws IllegalBalanceException {
+        // Not enough money in balance - throw IllegalBalanceException
+        if (amount > this.getBalance()){
+            throw new IllegalBalanceException("Insufficient funds for withdrawl in account!");
+        }
+        else if (amount >= 0.0) {
+            this.setBalance(this.getBalance() - amount);
+        }
+    }
+
+    /** Method for monthly management activities of a BankAccount object type.
+     *  Must be implemented by all extending subclasses.
+     *
+     */
+    public abstract monthlyManagement(){};
+
+    /** Returns a String representation of this BankAccount object
+     *
+     * @return This BankAccount in String form
+     */
+    @Override
+    public String toString() {
+        return String.format("number: %s%nAccount Owner: %s%nOwner ID: %s%nbalance: $%,.2f",
+                        getAccountNumber(), getAccountOwnerName(),
+                        getAccountOwnerId(), getBalance());
+    }
+
+    /** Compares 2 BankAccounts by their respective properties
+     *
+     * @param o The BankAccount to compare this BankAccount to
+     * @return - true if the BankAccounts are equal and false otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || !(o instanceof BankAccount)) {
+            return false;
+        }
+        else {
+            BankAccount ba = (BankAccount)o;
+            return ba.getBalance() == this.getBalance() &&
+                   ba.getAccountNumber() == this.getAccountNumber() &&
+                   ba.getAccountOwnerId() == this.getAccountOwnerId() &&
+                   ba.getAccountOwnerName() == this.getAccountOwnerName();
         }
     }
 }
---
+++ BankAccount/src/CheckingAccount.java
@@ -0,0 +1,27 @@
+public abstract class CheckingAccount extends BankAccount {
+
+    /******************************************************
+     *                      Constructors                  *
+     *****************************************************/
+    public CheckingAccount(String accountNumber, String accountOwnerName, String accountOwnerId, double balance) {
+        super(accountNumber, accountOwnerName, accountOwnerId, balance);
+    }
+
+    /******************************************************
+     *                      Methods                       *
+     *****************************************************/
+
+    /** Writes a check(deducts amount from this account in the given amount)
+     *
+     * @param checkAmount The amount which to deduct from this CheckingAccount(the amount on the check)
+     * @throws IllegalBalanceException if the check's amount is higher than this CheckingAccount's balance
+     */
+    public void writeCheck(double checkAmount) throws IllegalBalanceException{
+        if (checkAmount > this.getBalance()) {
+            throw new IllegalBalanceException("Check bounced. Insufficient funds in account!")
+        }
+        else {
+            this.setBalance(this.getBalance() - checkAmount);
+        }
+    }
+}
