b0da04bfce29ec0a4335eebb3a629358cda58518
Gad Maor <gadm@il.ibm.com>
2018-11-25 01:29:45 +0200

    BankAccountDriver start

--- BankAccount/src/BankAccountDriver.java
+++ BankAccount/src/BankAccountDriver.java
@@ -1,16 +1,15 @@
 public class BankAccountDriver {
     public static void main(String[] args) {
-        ServiceChargeChecking scc = null;
-        try {
-            scc = new ServiceChargeChecking("253874", "Gad Maor", "065965832", 21537);
-            //System.out.println(scc);
-            scc.withdraw(22000);
-            ServiceChargeChecking scc2 = new ServiceChargeChecking("253874", "Gad Maor", "065965832", 21537, 5);
-            System.out.println(scc2);
-            System.out.println(scc.equals(scc2));
-        }
-        catch (IllegalBalanceException e) {
-            System.err.printf("%nException: %s%n in account %s", e, scc);
+        BankAccount[] accounts = new BankAccount[5];
+
+        accounts[0] = new ServiceChargeChecking("748596", "James Bienstock", "678154962", 12000);
+        accounts[1] = new NoServiceChargeChecking("958613", "Demodard Galadriad", "348591765", 5000, 1500);
+        accounts[2] = new InterestChecking("916348", "John Whatshisface", "894535781", 300);
+        accounts[3] = new SavingsAccount("593485", "Jane Cookie-Dough", "251385496", 2200, 10);
+        accounts[4] = new HighInterestSavings("693258", "Johnnie Mnemonic", "546813759", 3000, 500);
+
+        for (BankAccount account : accounts) {
+            System.out.println(account);
         }
     }
 }
